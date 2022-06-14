dd194f0c8b593c8b2b5a9875e5b76ea057e9dcec
Emerson Medina <emersonmedina42@gmail.com>
2022-06-14 00:16:34 -0300

    desafio2

---
+++ ecommerce/src/components/Body.js
@@ -0,0 +1,16 @@
+import Card from "./Card";
+import './Body.css'; // Import the CSS file
+
+function Body() {
+    return (
+        <div className="body container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />  
+        </div>
+    );
+  }
+  
+  export default Body;
+  
---
+++ ecommerce/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import  './NavBar.css'
+
+function NavBar() {
+  return (
+    <header>
+      <nav className='navBar'>
+        <p>Inicio</p>
+        <p>Nosotros</p>
+        <p>Tienda</p>
+      </nav>
+    </header>
+  );
+}
+
+export default NavBar;
