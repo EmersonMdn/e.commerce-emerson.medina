9912c1cd768e9c5cd334743954b6628b1447a729
Emerson Medina <emersonmedina42@gmail.com>
2022-06-27 19:26:01 -0300

    Desafios realizados

--- ecommerce/src/components/ItemListContainer/ItemList/Item/ItemCount/ItemCount.js
+++ ecommerce/src/components/ItemListContainer/ItemList/Item/ItemCount/ItemCount.js
@@ -1,9 +1,30 @@
 import './ItemCount.css';
+import React, { useState } from 'react';
+
 
 function ItemCount() {
+    const [valor, setValor] = useState(1);
+
+    const sumar = () => {
+        if(valor < 5){
+            setValor(valor + 1);
+        }
+        
+    }
+    const restar = () => {
+        if(valor > 1){
+            setValor (valor - 1);
+        }
+    }
+
     return ( 
-    <div className='item-count'>
-        <button> - </button><p>Agregar al carro: 0</p><button> + </button> 
+    <div className='item-count-container'>
+        <div className='item-count'>
+           <button onClick={restar}> - </button><p> {valor} </p><button onClick={sumar}> + </button> 
+        </div>
+        <div className='addCart'>
+            <p><a className='addCart-btn'  href='##'>Agregar al carrito</a></p>
+        </div>
     </div>);
 }
 
